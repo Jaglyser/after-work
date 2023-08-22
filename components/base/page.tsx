3eafa3fe7778c8a036789aff9b36786e40e8a716
Joel Wallner Blomster <joel.wallner-blomster@seb.se>
2023-08-22 17:25:54 +0200

    fixed overflow again

--- components/base/page.tsx
+++ components/base/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Base({ children }: any) {
     return (
-        <div className="flex items-center justify-center">
-            <div className="relative w-[1440px] px-[120px] overflow-hidden">{children}</div>
+        <div className="flex items-center justify-center overflow-hidden">
+            <div className="relative w-[1440px] px-[120px]">{children}</div>
         </div>
     )
 }
